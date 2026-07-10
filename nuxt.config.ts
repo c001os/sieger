f97ab52fdfaad1c9820b1976296aafb7d42e223e
Tibor Farkas <tibor@previsionlab.hu>
2026-07-10 08:48:40 +0200

    favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -16,6 +16,15 @@ export default defineNuxtConfig({
         display: 'swap',
         subsets: ['latin', 'latin-ext'],
     },
+    app: {
+        head: {
+            link: [
+                { rel: 'icon', type: 'image/png', href: '/favicon.png' },
+                { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+                { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
+            ],
+        },
+    },
     colorMode: {
         preference: 'dark',
         fallback: 'dark',
